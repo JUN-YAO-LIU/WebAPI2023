fa731218b4de770b8f45ae4beba4b720ea9a10b3
unknown <jim.liu123@ehsn.com.tw>
2023-10-11 11:55:16 +0800

    first

---
+++ WebAPI2023/Areas/Products/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI2023.Areas.AreaName.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ WebAPI2023/TempStatic.cs
@@ -0,0 +1,6 @@
+﻿namespace WebAPI2023
+{
+    public class TempStatic
+    {
+    }
+}
